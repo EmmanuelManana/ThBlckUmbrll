0d6807604f2d1da195799b6de7949c461b5997bf
EmmanuelManana <emanana@student.wethinkcode.co.za>
2020-07-10 19:50:05 +0200

    to add client-side form-validation

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def register_user():
+    logged_in = True
     return render_template('home.html', logged_in = False)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('auth/register.html')
